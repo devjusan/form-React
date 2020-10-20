db841756f95f1e359d5489a92e8605ac4385c1e6
jnrplay <jnrplay@hotmail.com>
2020-10-20 01:12:10 -0300

    xd

--- src/App.js
+++ src/App.js
@@ -1,88 +1,27 @@
 import React from 'react';
-import Radio from './Form/Radio';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Contato from './Components/Contato';
+import Footer from './Components/Footer';
+import Header from './Components/Header';
+import Produtos from './Components/Produtos';
+import Produto from './Components/Produto';
 
-const perguntas = [
-  {
-    pergunta: 'Qual método é utilizado para criar componentes?',
-    options: [
-      'React.makeComponent()',
-      'React.createComponent()',
-      'React.createElement()',
-    ],
-    resposta: 'React.createElement()',
-    id: 'p1',
-  },
-  {
-    pergunta: 'Como importamos um componente externo?',
-    options: [
-      'import Component from "./Component"',
-      'require("./Component")',
-      'import "./Component"',
-    ],
-    resposta: 'import Component from "./Component"',
-    id: 'p2',
-  },
-  {
-    pergunta: 'Qual hook não é nativo?',
-    options: ['useEffect()', 'useFetch()', 'useCallback()'],
-    resposta: 'useFetch()',
-    id: 'p3',
-  },
-  {
-    pergunta: 'Qual palavra deve ser utilizada para criarmos um hook?',
-    options: ['set', 'get', 'use'],
-    resposta: 'use',
-    id: 'p4',
-  },
-];
 const App = () => {
-  const [respostas, setRespostas] = React.useState({
-    p1: '',
-    p2: '',
-    p3: '',
-    p4: '',
-  });
-
-  const [slide, setSlide] = React.useState(0);
-  const [resultado, setResultado] = React.useState(null);
-
-  function handleChange({ target }) {
-    setRespostas({ ...respostas, [target.id]: target.value });
-  }
-
-  function resultadoFinal() {
-    const corretas = perguntas.filter(
-      ({ id, resposta }) => respostas[id] === resposta,
-    );
-    setResultado(`Você acertou: ${corretas.length} de ${perguntas.length}`);
-  }
-
-  function handleClick() {
-    if (slide < perguntas.length - 1) {
-      setSlide(slide + 1);
-    } else {
-      setSlide(slide + 1);
-      resultadoFinal();
-    }
-  }
-
   return (
-    <form onSubmit={(event) => event.preventDefault()}>
-      {perguntas.map((pergunta, index) => (
-        <Radio
-          active={slide === index}
-          key={pergunta.id}
-          value={respostas[pergunta.id]}
-          onChange={handleChange}
-          {...pergunta}
-        />
-      ))}
-      {resultado ? (
-        <p>{resultado}</p>
-      ) : (
-        <button onClick={handleClick}>Próxima</button>
-      )}
-    </form>
+    <div className="App">
+      <BrowserRouter>
+        <Header />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Produtos />} />
+            <Route path="produto/:id" element={<Produto />} />
+            <Route path="contato" element={<Contato />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
+    </div>
   );
 };
 export default App;
